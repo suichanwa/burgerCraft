8056d72bbd74a4ada916a0ef4cf8bced69e708a7
suichanwa <augustinnegura746@gmail.com>
2023-12-13 09:15:01 -0800

    upd port

--- index.ts
+++ index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
-app.listen(3001, () => {
-  console.log('Server is running at http://localhost:3001');
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
-
 
 export default app;
